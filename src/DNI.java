0ae62837b653e5a32487e99c4f9337de9530e1ee
pauletik <pauletik@nobeboaguasolococacola.com>
2025-03-19 13:23:34 +0100

    cambiado codigo DNI a clase a parte

---
+++ src/DNI.java
@@ -0,0 +1,29 @@
+public class DNI {
+    public DNI() {}
+    /**
+     * Cmprueba DNI y su letra para ver si es correcto
+     * @param dni
+     * @param letra
+     * @return true si es correcto, false si no lo es
+     */
+    public static boolean comprobarDNI (String dni, char letra){
+        // calcula la letra del DNI
+        char letraCalculada = calcularLetraDNI(dni);
+        // compara la letra del DNI con la letra pasada por parámetro
+        return letraCalculada == letra;
+    }
+
+    /**
+     * Calcula la letra de un DNI
+     * @param dni
+     * @return la letra del DNI
+     */
+    public static char calcularLetraDNI (String dni) {
+        // array de caracteres con las letras del DNI
+        char[] letrasDNI = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'Q', 'S', 'V', 'H', 'L', 'C', 'K', 'E'};
+        // calcula el resto de dividir el DNI entre 23
+        int resto = Integer.parseInt(dni) % 23;
+        // devuelve la letra correspondiente al resto
+        return letrasDNI[resto];
+    }
+}
